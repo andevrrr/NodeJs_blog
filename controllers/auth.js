b85674dce1a0e60cc67e784bae2dc808f3012b52
Anton <kucherenko-anton1@mail.ru>
2023-08-16 17:22:49 +0300

    postLogin changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,72 +22,49 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const errors = validationResult(req);
 
-  if (!errors.isEmpty) {
-    console.log(errors.array());
-    return res.status(422).render("auth/login", {
-      pageTitle: "Login",
-      path: "/login",
-      errorMessage: errors.array()[0].msg,
-      validationErrors: [{ param: "email", param: "password" }],
-      oldInput: {
-        email: email,
-        password: password,
-      },
-    });
+  let loadedUser;
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    const error = new Error("A user does not exist");
+    error.statusCode = 401;
+    throw error;
   }
+  loadedUser = user;
+  try {
+    const isEqual = await bcrypt.compare(password, user.password);
 
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        return res.status(422).render("auth/login", {
-          pageTitle: "Login",
-          path: "/login",
-          errorMessage: "Such email is not registered.",
-          validationErrors: [{ param: "email", param: "password" }],
-          oldInput: {
-            email: email,
-            password: password,
-          },
-        });
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        email: loadedUser.email,
+        userId: loadedUser._id.toString(),
+      },
+      "somesecrettoken",
+      {
+        expiresIn: "1h",
       }
+    );
 
-      bcrypt
-        .compare(password, user.password)
-        .then((doMatch) => {
-          console.log("loggedIn");
-          if (doMatch) {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            return req.session.save((err) => {
-              console.log(err);
-              res.redirect("/");
-            });
-          }
-          return res.status(422).render("auth/login", {
-            path: "/login",
-            pageTitle: "Login",
-            errorMessage: "Invalid Password",
-            validationErrors: [{ param: "email", param: "password" }],
-            oldInput: {
-              email: email,
-              password: password,
-            },
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          return res.redirect("/login");
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.redirect("/login");
+    res.status(200).json({
+      token: token,
+      userId: loadedUser._id.toString(),
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.getSignUp = (req, res, next) => {

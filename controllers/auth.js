90d8289aec55e57c3c9ab6e6af9c08405c7f2ddc
Anton <kucherenko-anton1@mail.ru>
2023-08-16 17:14:42 +0300

    jwt installed & postSignUp changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -55,9 +56,10 @@ exports.postLogin = (req, res, next) => {
         });
       }
 
-      bcrypt.compare(password, user.password)
-        .then(doMatch => {
-            console.log('loggedIn');
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          console.log("loggedIn");
           if (doMatch) {
             req.session.isLoggedIn = true;
             req.session.user = user;
@@ -110,51 +112,62 @@ exports.getSignUp = (req, res, next) => {
   });
 };
 
-exports.postSignUp = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+exports.postSignUp = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
-    return res.status(422).render("auth/signup", {
-      pageTitle: "SignUp",
-      path: "signup",
-      errorMessage: errors.array()[0].msg,
-      validationErrors: [{ param: "email", param: "password" }],
-      oldInput: {
-        email: email,
-        password: password,
-        confirmPassword: req.body.confirmPassword,
-      },
-    });
+    const error = new Error("Validation failed.");
+    error.statusCode = 422;
+    error.data = error.array();
+    throw error;
   }
 
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPassword) => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-      });
+  // if (!errors.isEmpty()) {
+  //   console.log(errors.array());
+  //   return res.status(422).render("auth/signup", {
+  //     pageTitle: "SignUp",
+  //     path: "signup",
+  //     errorMessage: errors.array()[0].msg,
+  //     validationErrors: [{ param: "email", param: "password" }],
+  //     oldInput: {
+  //       email: email,
+  //       password: password,
+  //       confirmPassword: req.body.confirmPassword,
+  //     },
+  //   });
+  // }
 
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect("/login");
-    })
-    .catch((err) => {
-      console.log(err);
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      name: name,
     });
+    const result = await user.save();
+    res.status(201).json({
+      message: "User created!",
+      userId: result._id,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.postLogout = (req, res, next) => {
-    if(req.session) {
-        req.session.destroy(err => {
-            console.log(err);
-            res.redirect('/');
-        })
-    } else {
-        console.log('Not logged in');
-    }
-}
+  if (req.session) {
+    req.session.destroy((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+  } else {
+    console.log("Not logged in");
+  }
+};
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,35 @@
+// module.exports = (req, res, next) => {
+//     if (!req.session.isLoggedIn) {
+//         return res.redirect('/login');
+//     }
+//     next();
+// }
+
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        const error = new Error('Not authenticated');
+        error.statusCode = 401;
+        throw error;
     }
+
+    const token = req.get('Authorization').split(' ')[1];
+    let decodedToken;
+
+    try {
+        decodedToken = jwt.verify(token, 'somesecrettoken');
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+
+    if (!decodedToken) {
+        const error = new Error('Not authenticated');
+        error.statusCode = 401;
+        throw error;
+    }
+    req.userId = decodedToken.userId;
     next();
-}
+};
